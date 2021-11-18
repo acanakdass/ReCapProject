9f6e454730be709c35c633dd725b9e96f338afc3
ahmetcanakdas <acanakdas@gmail.com>
2021-11-19 00:36:37 +0300

    getCurrentUser

---
+++ DataAccess/Migrations/20211006225642_rentaltableedit.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class rentaltableedit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDelivered",
+                table: "Rentals",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDelivered",
+                table: "Rentals");
+        }
+    }
+}
---
+++ Entities/DTOs/UserLoggedInDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Entities.DTOs
+{
+    public class UserLoggedInDto
+    {
+        public UserLoggedInDto()
+        {
+        }
+    }
+}
